3f80d90b503222567334d53061abe7e8ccdae71d
Igor Martayan <igor@martayan.org>
2023-05-23 17:21:05 +0200

    Add minimal examples for the construction dense / sparse / hashset De Bruijn graphs

---
+++ examples/dense.rs
@@ -0,0 +1,23 @@
+use std::env;
+use tinydbg::dbg::{Dbg, DbgBuilder, DenseDbgBuilder};
+use tinydbg::kmer::{Canonical, IntKmer, Kmer};
+use tinydbg::reads::{Fasta, ReadProcess};
+
+const K: usize = 15;
+type T = u32;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = args.get(1).expect("No filename given").as_str();
+    let reads = Fasta::from_file(filename);
+    let mut builder = DenseDbgBuilder::<K, T>::new();
+    reads.process(|bytes| {
+        IntKmer::<K, T>::iter_from_chars(bytes).for_each(|kmer| builder.insert(kmer.canonical()));
+    });
+    let dbg = builder.build();
+    let reads = Fasta::from_file(filename);
+    reads.process(|bytes| {
+        IntKmer::<K, T>::iter_from_chars(bytes)
+            .for_each(|kmer| assert!(dbg.contains(kmer.canonical())));
+    });
+}
